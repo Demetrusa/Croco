6a33c0e3115ae081343c774dbde2cf5a763e148c
Sanraku95 <demetriusgit@gmail.com>
2024-09-24 18:02:02 +0400

    feat: add miniheader

--- Croco-bet/src/components/HorizontalHeader/HorizontalHeader.tsx
+++ Croco-bet/src/components/HorizontalHeader/HorizontalHeader.tsx
@@ -1,11 +1,80 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
 import "./HorizontalHeader.css";
-import VerticalHeader from "../VerticalHeader/VerticalHeader";
+import "swiper/css/navigation";
+import PromoImg1 from "./hHeader-img/promo-img-1.png";
+import PromoImg2 from "./hHeader-img/promo-img-2.png";
+import PromoImg3 from "./hHeader-img/promo-img-3.png";
+import PromoImg4 from "./hHeader-img/promo-img-4.png";
+import PromoImg5 from "./hHeader-img/promo-img-5.png";
+import PromoImg6 from "./hHeader-img/promo-img-6.png";
+import PromoImg7 from "./hHeader-img/promo-img-7.webp";
+import PromoImg8 from "./hHeader-img/promo-img-8.png";
+import RegBtn from "../regBtn/RegBtn";
 
 const HorizontalHeader: React.FC = () => {
   return (
     <div className="horizontal-header">
-      <VerticalHeader />
+      <div className="horizontal-header__swiper">
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={10}
+          slidesPerView={4.2}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <div className="promoSlide">
+              <img src={PromoImg1} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="promoSlide">
+              <img src={PromoImg2} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="promoSlide">
+              <img src={PromoImg3} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="promoSlide">
+              <img src={PromoImg4} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="promoSlide">
+              <img src={PromoImg5} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="promoSlide">
+              <img src={PromoImg6} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="promoSlide">
+              <img src={PromoImg7} alt="" />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="promoSlide">
+              <img src={PromoImg8} alt="" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        <div className="swiper-button-next"></div>
+        <div className="swiper-button-prev"></div>
+      </div>
+      <RegBtn />
+      <p>aloha</p>
     </div>
   );
 };
---
+++ Croco-bet/src/components/regBtn/RegBtn.tsx
@@ -0,0 +1,11 @@
+import "./RegBtn.css";
+
+const RegBtn: React.FC = () => {
+  return (
+    <div className="regBtn">
+      <p>რეგისტრაცია</p>
+    </div>
+  );
+};
+
+export default RegBtn;
